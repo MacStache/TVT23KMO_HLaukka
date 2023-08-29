e5a39bed6bd59b1f8498b7891cdf88ab113f48c5
Henri Laukka <henri.laukka@gmail.com>
2023-08-29 17:49:44 +0300

    Week Assignment 1 Commenting
    Added comments to week assignment 1 so it's easier to grade

--- Object_Oriented_Coding/Week1_Random_Number_Quiz/main.cpp
+++ Object_Oriented_Coding/Week1_Random_Number_Quiz/main.cpp
@@ -4,41 +4,46 @@
 
 using namespace std;
 
+//alustetaan muuttujat
 int maxnum;
 int luku;
 int aluku = 0;
 
+//Pelifunktio
 int game(int maxnum) {
 
-    srand(maxnum);
-    for (int i=0; i<1; i++){
-        int satunnaisluku = 1 + rand() % maxnum;
+    srand(maxnum);                                  //Alustetaan random number seed käyttäjän antaman maxnum muuttujan mukaan
+
+    for (int i=0; i<1; i++){                        //For looppi, jossa peli pyöritetään
+        int satunnaisluku = 1 + rand() % maxnum;    /*Pyöräytetään satunnaisluku. Aloitetaan "1+" kohdassa ykkösestä (ettei arvata nollia)
+                                                    ja asetetaan maksimiksi käyttäjän syöttämä maxnum. rand() arpoo kaikkien näiden välillä olevien
+                                                    väliltä */
         cout << "Syota arvattava luku!" << endl;
-        cin >> luku;
-        if (luku == satunnaisluku) {
-            cout << "Oikein!" << endl;
-            aluku = aluku + 1;
-            return aluku;
+        cin >> luku;                                //Käyttäjä syöttää arvauksen
+        if (luku == satunnaisluku) {                //Jos käyttäjän luku on sama kuin rand() arpoma luku niin
+            cout << "Oikein!" << endl;              //Tulostetaan näytölle "Oikein!"
+            aluku = aluku + 1;                      //Lisätään aluku (arvauskerrat) muuttujaan yksi arvaus
+            return aluku;                           //Palautetaan aluku main -funktioon
         }
-        else if (luku <= satunnaisluku){
-            cout << "Luku on suurempi!" << endl;
-            aluku = aluku + 1;
-            game(maxnum);
+        else if (luku <= satunnaisluku){            //Jos ei mene oikein niin tarkistetaan, että onko luku pienempi kuin satunnaisluku
+            cout << "Luku on suurempi!" << endl;    //Jos arvattu luku on pienempi niin kerrotaan, että etsittävä luku on suurempi
+            aluku = aluku + 1;                      //Lisätään aluku (arvauskerrat) muuttujaan yksi arvaus
+            game(maxnum);                           //Palautaan game -funktion alkuun ja palautetaan sinne taas maxnum -muuttujan sisältö
         }
-        else if (luku >= satunnaisluku){
-            cout << "Luku on pienempi!" << endl;
-            aluku = aluku + 1;
-            game(maxnum);
+        else if (luku >= satunnaisluku){            //Jos ei mene oikein niin tarkistetaan, että onko luku suurempi kuin satunnaisluku
+            cout << "Luku on pienempi!" << endl;    //Jos arvattu luku on surempi, niin kerrotaan, että etsittävä luku on pienempi
+            aluku = aluku + 1;                      //Lisätään aluku (arvauskerrat) muuttujaan yksi arvaus
+            game(maxnum);                           //Palautaan game -funktion alkuun ja palautetaan sinne taas maxnum -muuttujan sisältö
         }
     }
 }
 
 int main()
 {
-    cout << "Syota arvottava maksimiluku" << endl;
-    cin >> maxnum;
-    game(maxnum);
-    cout << "Arvausten maara: " << aluku << endl;
+    cout << "Syota arvottava maksimiluku" << endl;  //Kysytään käyttäjältä maksimiluku, jota arvotaan
+    cin >> maxnum;                                  //Otetaan syöte vastaan
+    game(maxnum);                                   //Hypätään game -funktioon ja viedään sinne maxnum -muuttujan arvo
+    cout << "Arvausten maara: " << aluku << endl;   //Palataan game -funktiosta ja palautetaan sieltä arvausten määrä, joka tulostetaan näytölle
 
 }
 
