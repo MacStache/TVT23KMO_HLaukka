699f03407a7ae49f252a91d1eb56232806e6566a
Henri Laukka <henri.laukka@gmail.com>
2023-08-29 17:32:03 +0300

    Week Assignment 1
    User number input and random number generator quiz game

--- Object_Oriented_Coding/Week1_Random_Number_Quiz/main.cpp
+++ Object_Oriented_Coding/Week1_Random_Number_Quiz/main.cpp
@@ -1,9 +1,45 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
+int maxnum;
+int luku;
+int aluku = 0;
+
+int game(int maxnum) {
+
+    srand(maxnum);
+    for (int i=0; i<1; i++){
+        int satunnaisluku = 1 + rand() % maxnum;
+        cout << "Syota arvattava luku!" << endl;
+        cin >> luku;
+        if (luku == satunnaisluku) {
+            cout << "Oikein!" << endl;
+            aluku = aluku + 1;
+            return aluku;
+        }
+        else if (luku <= satunnaisluku){
+            cout << "Luku on suurempi!" << endl;
+            aluku = aluku + 1;
+            game(maxnum);
+        }
+        else if (luku >= satunnaisluku){
+            cout << "Luku on pienempi!" << endl;
+            aluku = aluku + 1;
+            game(maxnum);
+        }
+    }
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    cout << "Syota arvottava maksimiluku" << endl;
+    cin >> maxnum;
+    game(maxnum);
+    cout << "Arvausten maara: " << aluku << endl;
+
 }
+
+
