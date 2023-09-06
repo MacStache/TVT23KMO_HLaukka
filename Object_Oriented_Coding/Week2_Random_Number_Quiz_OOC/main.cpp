661218764aff7f29ba3ab948ee52d3115a9d45ee
Henri Laukka <123492715+MacStache@users.noreply.github.com>
2023-09-06 16:49:00 +0300

    Update main.cpp with comments

--- Object_Oriented_Coding/Week2_Random_Number_Quiz_OOC/main.cpp
+++ Object_Oriented_Coding/Week2_Random_Number_Quiz_OOC/main.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-    Game gameObject(10);
-    gameObject.play();
+    Game gameObject(10);  //Viedään game-funktioon maksimiluvuksi 10
+    gameObject.play(); //Kutsutaan pelifunktiota
     return 0;
 }
