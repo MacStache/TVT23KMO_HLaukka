b673945ffcbc5d18fbfd5e3fe89ec322c3ec5d37
Henri Laukka <henri.laukka@gmail.com>
2023-09-06 16:55:40 +0300

    updated with comments
    updated files with comments for clarity

--- Object_Oriented_Coding/Week2_Random_Number_Quiz_OOC/game.h
+++ Object_Oriented_Coding/Week2_Random_Number_Quiz_OOC/game.h
@@ -9,34 +9,34 @@ using namespace std;
 class Game
 {
 public:
-    Game(int maxNumber)
+    Game(int maxNumber) //alustetaan Game -funktio
         : maxNumber(maxNumber), numOfGuesses(0){
-        srand(time(0));
-        randomNumber = 1 + rand() % maxNumber;
-        cout << "GAME CONSTRUCTOR: object initialized with " << maxNumber << " as a maximum value" << endl;
+        srand(time(0)); //asetetaan random number generaattorin siemen ajan perusteella
+        randomNumber = 1 + rand() % maxNumber; //arvotaan numero ja varmistetaan että arvotaan vain 1-10 lukuja
+        cout << "GAME CONSTRUCTOR: object initialized with " << maxNumber << " as a maximum value" << endl; //kerrotaan, että constructori on tehnyt tehtävänsä
     }
     ~Game(){
-        cout << "GAME DESTRUCTOR: object cleared from stack memory"<< endl;
+        cout << "GAME DESTRUCTOR: object cleared from stack memory"<< endl;  //kerrotaan, että destructor on tehnyt tehtävänsä
     }
 
-    void play(){
+    void play(){ //pelifunktio
 
-        while (true) {
-            cout << "Give your guess between 1-" << maxNumber << endl;
-            cin >> playerGuess;
-            numOfGuesses++;
+        while (true) {  //while -looppi pyörii niin kauan kuin sille sanotaa, että älä pyöri
+            cout << "Give your guess between 1-" << maxNumber << endl; //pyydetään arvaamaan numeroita välillä 1-(maxNumber)
+            cin >> playerGuess; //ilmoitetaan mitä arvattiin
+            numOfGuesses++; //lisätään arvauksiin yksi arvauskerta lisää
 
-            if (playerGuess == randomNumber) {
-                cout << "Your guess is right = " << randomNumber << endl;
+            if (playerGuess == randomNumber) { //jos pelaaja arvaa oikein
+                cout << "Your guess is right = " << randomNumber << endl; //kerrotaan, että arvattiin oikein ja, että mitä lukua arvattiin
                 break;  // Lopeta peli, jos arvaus on oikein
-            } else if (playerGuess < randomNumber) {
-                cout << "Your guess is too small" << endl;
+            } else if (playerGuess < randomNumber) { //jos vastaus on liian pieni
+                cout << "Your guess is too small" << endl; //kerrotaan, että vastaus on liian pieni
             } else {
-                cout << "Your guess is too big" << endl;
+                cout << "Your guess is too big" << endl; //muussa tapauksessa kerrotaan, että vastaus on liian suuri
             }
         }
 
-        printGameResult();
+        printGameResult(); //kutsutaan pelin tuloksen kertovaa funktiota
 
     }
 
@@ -46,8 +46,8 @@ private:
     int randomNumber;
     int numOfGuesses;
 
-    void printGameResult(){
-        cout << "You guessed the right answer = "<< randomNumber << " with "<< numOfGuesses << " guesses" << endl;
+    void printGameResult(){ //pelin tuloksen kertova funktio
+        cout << "You guessed the right answer = "<< randomNumber << " with "<< numOfGuesses << " guesses" << endl; //kerrotaan, että arvattiin oikein, mitä lukua arvattiin ja montako kertaa arvattiin
 
     }
 
