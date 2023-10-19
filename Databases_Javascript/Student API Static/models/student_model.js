823b666fb7d84378598c55a9a5c57435013330e9
Henkka Laukka <123492715+MacStache@users.noreply.github.com>
2023-10-19 17:38:07 +0300

    Tietokantayhteys + funktioita
    Tietokantayhteys toimii, opiskelijan poistaminen ja päivittäminen toimii.

--- Databases_Javascript/Student API Static/models/student_model.js
+++ Databases_Javascript/Student API Static/models/student_model.js
@@ -4,20 +4,21 @@ const student={
     getAllStudents: function(callback){
         return db.query("SELECT * FROM student", callback);
     },
-    getOneStudent: function(id, callback){
+    getOneStudent: function(id,callback){
         return db.query("SELECT * FROM student where id_student=?", [id], callback);
     },
     addStudent: function(newData,callback){
-        // return "Lisätään uusi opiskelija (insert into...";
         return db.query("INSERT INTO student values(?,?,?)", 
         [newData.id_student, newData.firstname, newData.lastname], callback);
     },
-    updateStudent: function(id){
-        return "Päivitetään opiskelija, jonka id="+id;
+    updateStudent: function(id,updateData,callback){
+        return db.query("UPDATE student set firstname=?, lastname=? WHERE id_student=?", 
+        [updateData.firstname, updateData.lastname, id], callback);
     },
-    deleteStudent: function(id){
-        return "Poistetaan opiskelija, jonka id="+id;
-    },
+    deleteStudent: function(id,callback){
+        return db.query("DELETE FROM student WHERE id_student=?", 
+        [id], callback);
+    }
 };
 
 module.exports=student;
