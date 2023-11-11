91c422099f6060899bef0d960a5e388144f99648
Henkka Laukka <123492715+MacStache@users.noreply.github.com>
2023-11-11 09:50:37 +0200

    user taulun crud operaatiot toimii

--- Databases_Javascript/Student API Static/app.js
+++ Databases_Javascript/Student API Static/app.js
@@ -1,14 +1,20 @@
 const express=require('express');
 //const port=3000;
 const studentRouter=require('./controllers/student');
+const userRouter=require('./controllers/user');
+
 const app=express();
 const dotenv=require('dotenv');
+const user = require('./models/user_model');
 dotenv.config();
 const port=process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-app.use('/student',studentRouter);
+
+app.use ('/student', studentRouter);
+app.use ('/user', userRouter);
+
 app.listen(port,function(){
     console.log("sovellus kuuntelee porttia "+port);
 });
