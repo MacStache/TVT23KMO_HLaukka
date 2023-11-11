2ebab50f2005b7f3250b54a7af24d42506c4a343
Henkka Laukka <123492715+MacStache@users.noreply.github.com>
2023-11-11 10:30:23 +0200

    login systeemi toimii

--- Databases_Javascript/Student API Static/app.js
+++ Databases_Javascript/Student API Static/app.js
@@ -2,6 +2,7 @@ const express=require('express');
 //const port=3000;
 const studentRouter=require('./controllers/student');
 const userRouter=require('./controllers/user');
+const loginRouter=require('./controllers/login');
 
 const app=express();
 const dotenv=require('dotenv');
@@ -14,6 +15,7 @@ app.use(express.urlencoded({extended:false}));
 
 app.use ('/student', studentRouter);
 app.use ('/user', userRouter);
+app.use ('/login', loginRouter);
 
 app.listen(port,function(){
     console.log("sovellus kuuntelee porttia "+port);
