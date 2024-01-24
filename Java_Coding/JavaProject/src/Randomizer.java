236e591604453b7c0b4a5ef8d21b0c04b5ea75b4
Henkka Laukka <123492715+MacStache@users.noreply.github.com>
2024-01-24 18:19:25 +0200

    Java coding exercise: Randomizer I created a randomizer that will return a random index from an array and print it to the console. Once it has printed out the index of the array it will delete that index and repeat the proces until the array is empty.
    It will also group the indexes into groups of 4 and print a
    new line after each group of 4 indexes.

---
+++ Java_Coding/JavaProject/src/Randomizer.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Randomizer {
+   
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("Henkka Laukka");
+        names.add("Jooseppi Joukahainen");
+        names.add("Jaska Jantteri");
+        names.add("Teppo Testi");
+        names.add("Matti Meikäläinen");
+        names.add("Maija Meikäläinen");
+        names.add("Mikko Mallikas");
+        names.add("Aila Antelias");
+        names.add("Pekka Pouta");
+        names.add("Jorma Jormalainen");
+        names.add("Kerma Kermalainen");
+        names.add("Kalle Kalamies");
+
+        for (int i = 0; i < 12; i++) {
+        
+        Random random = new Random();
+        int randomIndex = random.nextInt(names.size());
+
+        if (i == 4 || i == 8) {
+            System.out.println("");
+        }
+
+        System.out.println(names.get(randomIndex));
+        names.remove(randomIndex);
+        
+        }
+
+    }
+
+}
