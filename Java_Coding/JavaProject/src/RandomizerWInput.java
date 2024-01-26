1362e00f36e6ca9c8f42e6577b9cf812b62c05ee
Henkka Laukka <123492715+MacStache@users.noreply.github.com>
2024-01-26 19:13:54 +0200

    Added a file writing functionality The script will now ask the user for a text file name and create that file. It will then ask the user for random information (numbers, age, names, etc.) and write the generated information to that file.

--- Java_Coding/JavaProject/src/RandomizerWInput.java
+++ Java_Coding/JavaProject/src/RandomizerWInput.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
@@ -7,6 +10,11 @@ public class RandomizerWInput {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> names = new ArrayList<>();
+
+        System.out.println("Please enter filename:");
+        String filename = scanner.nextLine();
+
+        Createfile(filename);
 
         System.out.println("Enter names, one per line."
         + "\nBlank line ends the process."
@@ -29,14 +37,39 @@ public class RandomizerWInput {
 
         if (rowCounter == 4) {
             System.out.println("");
+            String nameToWrite = "";
+            WriteToFile(filename, nameToWrite);
             rowCounter = 0;
         }
-
         System.out.println(names.get(randomIndex));
+        String nameToWrite = names.get(randomIndex);
+        WriteToFile(filename, nameToWrite); 
         names.remove(randomIndex);
         rowCounter++;
         }
-
     }
 
+    public static void Createfile(String filename) {
+        File file = new File(filename + ".txt");
+        try {
+            if (file.createNewFile()) {
+                System.out.println("File created: " + file.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void WriteToFile(String filename, String nameToWrite) {
+        try {
+            FileWriter myWriter = new FileWriter(filename + ".txt", true);
+            myWriter.write(nameToWrite + "\n");
+            myWriter.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
 }
