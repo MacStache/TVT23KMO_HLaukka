e91b68d233827a277dc2cae7081fcf44f992933b
Henkka Laukka <123492715+MacStache@users.noreply.github.com>
2024-08-26 18:46:13 +0300

    Currency Calculator exercise done

--- Web_And_Hybrid/CurrencyCalculator/App.js
+++ Web_And_Hybrid/CurrencyCalculator/App.js
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [euros, setEuros] = useState('')
+  const [pounds, setPounds] = useState(0)
+
+  const handleTextChange = (text) => {
+    setEuros(text)
+    const result = text.replace(',','.') * 0.9
+    setPounds(result)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.field}>Euros</Text>
+      <TextInput
+        style={styles.field}
+        value={euros}
+        onChangeText={handleTextChange}
+        keyboardType='decimal-pad'
+      />
+      <Text style={styles.field}>Pounds</Text>
+      <Text style={styles.field}>{pounds.toFixed(2)}</Text>
     </View>
   );
 }
@@ -13,8 +29,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
+    marginLeft: 10
   },
+  field: {
+    marginBottom: 10
+  }
 });
